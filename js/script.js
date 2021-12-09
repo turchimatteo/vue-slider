0053d6d717e09c5e51f7270330e062788a66ff90
turchimatteo <turchi12@gmail.com>
2021-12-09 15:03:51 +0100

    correzione struttura vue(da terminare)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/* Descrizione:
+Partendo dal markup della versione svolta in js plain, rifare lo slider ma questa volta usando Vue.
+Se volete potete partire dalla mia repo dell'esercizio slider: https://github.com/AlessandroSCPse/js-array-carousel
+Bonus:
+1- al click su una thumb, visualizzare in grande l'immagine corrispondente
+2- applicare l'autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+3- quando il mouse va in hover sullo slider, bloccare l'autoplay e farlo riprendere quando esce */
+
+
 const items = [
     'img/01.jpg',
     'img/02.jpg',
@@ -22,83 +31,57 @@ const text = [
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ];
 
-// Popolo immagini e thumbnails dagli array
-const bigImagesContainer = document.querySelector('.big-images-container');
-const thumbsContainer = document.querySelector('.thumbs');
 
-for( let i = 0; i < items.length; i++ ) {
-    // Quali variabili mi servono per popolare il mio nuovo div ad ogni ciclo?
-    // Path
-    const thisPath = items[i];
-    // Alt/Titolo
-    const thisTitle = title[i];
-    // Testo/Descrizione
-    const description = text[i];
+Vue.config.devtools = true;
 
-    // Popolare il container big images
-    const newDiv = `
-    <div class="single-image">
-        <img src="${thisPath}" alt="${thisTitle}">
-
-        <div class="single-image-text">
-            <h3>${thisTitle}</h3>
-            <p>${description}</p>
-        </div>
-    </div>
-    `;
-
-    bigImagesContainer.innerHTML += newDiv;
-
-    // Popolare thumbnails a destra
-    const newThumbnail = `
-    <div class="single-thumb">
-        <img src="${thisPath}" alt="${thisTitle}">
-    </div>
-    `;
-
-    thumbsContainer.innerHTML += newThumbnail;
-}
-
-// Definire l'elemento di default
-let activeItem = 0;
-const allBigImages = document.getElementsByClassName('single-image');
-const allThumbnails = document.getElementsByClassName('single-thumb');
-allBigImages[activeItem].classList.add('active');
-allThumbnails[activeItem].classList.add('active');
-
-// Eventi sulle arrows (con l'idea di spostare la classe active all'elemento precedente o successiv)
-const arrowDown = document.querySelector('.down');
-arrowDown.addEventListener('click', function() {
-    // Rimuovo la classe active alla big image e alla thumbnail corrente
-    allBigImages[activeItem].classList.remove('active');
-    allThumbnails[activeItem].classList.remove('active');
-
-    // Aumento activeImage di 1
-    if( activeItem < allBigImages.length - 1 ) {
-        activeItem++;
-    } else {
-        activeItem = 0;
+const app = new Vue(
+    {
+        el: '#root',
+        data: {
+            activeSlide: 0,
+            slides: [
+                {
+                    image: 'img/01.jpg',
+                    title: 'Svezia',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.'
+                },
+                {
+                    image: 'img/02.jpg',
+                    title: 'Svizzera',
+                    text: 'Lorem ipsum'
+                },
+                {
+                    image: 'img/03.jpg',
+                    title: 'Gran Bretagna',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit.'
+                },
+                {
+                    image: 'img/04.jpg',
+                    title: 'Germania',
+                    text: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                },
+                {
+                    image: 'img/05.jpg',
+                    title: 'Paradise',
+                    text: 'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+                }
+            ]
+        },
+        methods: {
+            showNextSlide: function(){
+                if(this.activeSlide < this.slides.length - 1){
+                    this.activeSlide++;
+                } else{
+                    this.activeSlide = 0;
+                }                
+            },
+            showPrevSlide: function(){
+                if(this.activeSlide > 0){
+                    this.activeSlide--;
+                }else{
+                    this.activeSlide = this.slides.length -1;
+                }   
+            }
+        }
     }
-
-    // Aggiungo la classe active agli elementi big image e thumbnail che hanno il nuovo indice
-    allBigImages[activeItem].classList.add('active');
-    allThumbnails[activeItem].classList.add('active');
-});
-
-const arrowUp = document.querySelector('.up');
-arrowUp.addEventListener('click', function() {
-    // Rimuovo la classe active alla big image e alla thumbnail corrente
-    allBigImages[activeItem].classList.remove('active');
-    allThumbnails[activeItem].classList.remove('active');
-
-    // Decrementare activeImage di 1
-    if( activeItem > 0 ) {
-        activeItem--;
-    } else {
-        activeItem = allBigImages.length - 1;
-    }
-
-    // Aggiungo la classe active agli elementi big image e thumbnail che hanno il nuovo indice
-    allBigImages[activeItem].classList.add('active');
-    allThumbnails[activeItem].classList.add('active');
-});
+);
